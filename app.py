da01d60f5319b3af2534558be65e96f0186e7176
Javier <javucho1000@yahoo.com.mx>
2021-09-10 01:15:41 +0800

    Log static path

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from flask_restful import Api, Resource, reqparse
 #from flask_cors import CORS #comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
 
-#app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-app = Flask(__name__, static_url_path='', static_folder=os.path.abspath('frontend/build'))
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+#app = Flask(__name__, static_url_path='', static_folder=os.path.abspath('frontend/build'))
 #CORS(app) #comment this on deployment
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
 def serve(path):
     app.logger.warning('Accessing %s', path)
+    app.logger.warning('Static folder %s', app.static_folder)
     return send_from_directory(app.static_folder,'index.html')
 
 api.add_resource(HelloApiHandler, '/flask/hello')
